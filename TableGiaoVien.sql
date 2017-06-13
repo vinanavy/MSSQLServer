8075a2515221b069b585c0d9a970c6525f30a246
Vina Navy <vinanavy@gmail.com>
2017-06-13 22:46:25 +0700

    DataType

--- TableGiaoVien.sql
+++ TableGiaoVien.sql
@@ -4,3 +4,10 @@ CREATE TABLE GiaoVien
 	MaGV NVARCHAR(10),
 	TenGV NVARCHAR(30)
 )
+GO
+
+INSERT INTO dbo.GiaoVien
+        ( MaGV, TenGV )
+VALUES  ( N'', -- MaGV - nvarchar(10)
+          N''  -- TenGV - nvarchar(30)
+          )
---
+++ UseDataType.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE UseDataType
+(
+	MaSo INT,
+	Ten NVARCHAR(10),
+	NgaySinh DATE,
+	GioiTinh BIT,
+	DiaChi CHAR(20),
+	TienLuong FLOAT
+)
+GO
+
+INSERT dbo.UseDataType
+        ( MaSo ,
+          Ten ,
+          NgaySinh ,
+          GioiTinh ,
+          DiaChi ,
+          TienLuong
+        )
+VALUES  ( 1 , -- MaSo - int
+          N'Đạt' , -- Ten - nvarchar(10)
+          '19930913' , -- NgaySinh - date
+          1 , -- GioiTinh - bit
+          'Đà Nẵng' , -- DiaChi - char(20)
+          10000  -- TienLuong - float
+        )
